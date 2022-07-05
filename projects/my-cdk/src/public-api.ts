0e8bdf4ccb86caf4633bc7ccee435c7703853437
Wojciech Parys <wojciech.parys@nexocode.com>
2022-07-05 13:55:50 +0200

    Remove top-level exports support

--- projects/my-cdk/src/public-api.ts
+++ projects/my-cdk/src/public-api.ts
@@ -1,7 +1,10 @@
 /*
- * Public API Surface of my-cdk
+ * Public API Surface of @my-org/my-cdk
+ * notice that the internal utils are NOT exported from top level public-api.ts file
+ *
+ * You can add top-level exports support with exporting modules from here like
+ * export * from '@my-org/my-cdk/avatar';
  */
 
-export * from './lib/my-cdk.service';
-export * from './lib/my-cdk.component';
-export * from './lib/my-cdk.module';
+// https://github.com/ng-packagr/ng-packagr/issues/1655
+export default void 0;
